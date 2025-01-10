cde6fdb9aca576cac42fe4173b46163eacd682cc
Pranav Misra <138226737+MIZBOB11@users.noreply.github.com>
2025-01-10 19:39:52 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // Make sure the path to App.js is correct
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
